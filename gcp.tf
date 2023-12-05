b4b7b3d555ba9b2bf93867413de4626acdd9c389
asdeffg <sjk1364@naver.com>
2023-12-05 10:49:35 +0900

    gcp수정본

--- gcp.tf
+++ gcp.tf
@@ -1,12 +1,3 @@
-# terraform {
-    # required_providers {
-      #   google = {
-     #        source = "hashicorp/google"
-    #         version = "4.51.0"
-   #      }
-  #   }
- # }
-
 provider "google" {
     credentials = file("gcp_key.json")
 
@@ -15,17 +6,14 @@ provider "google" {
     zone    = "us-central1-a"
 }
 
-resource "google_compute_network" "vpc_network" {
-    name = "terraform-network"
-}
 
 resource "google_compute_instance" "vm_instance" {
-    name         = "terraform-instance"
+    name         = "terraform-instance2"
     machine_type = "f1-micro"
     //ssh-keys = "root:${file("/C:/Users/Admin/Downloads/multi-cloud-406002.pub")}"
-    metadata = {
-        ssh-keys = "root:${file("../../Downloads/multi-cloud-406002.pub")}"
-    }
+    // metadata = {
+    //     ssh-keys = "root:${file("${path.module}/.ssh/multi-cloud-406002.pub.")}"
+    // }
 
     boot_disk {
         initialize_params {
@@ -34,28 +22,13 @@ resource "google_compute_instance" "vm_instance" {
     }
 
     network_interface {
-        network = google_compute_network.vpc_network.name
+        network = "default"
         access_config {
         }
     }
 }
 
-resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc_network.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
-}
 
 output "gcp_vm_public_ip" {
   value = google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip
 }
-
-
-
