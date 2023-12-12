742d2a0f7efae04d31ee6390960b7d14afc6fa33
jhc1010 <149353473+jhc1010@users.noreply.github.com>
2023-12-12 09:26:39 +0900

    Update ncloud.tf

--- ncloud.tf
+++ ncloud.tf
@@ -1,87 +1,85 @@
-// # Terraform 초기화
-// terraform {                                                                   # Terraform 설정의 시작을 알리는 부분
-//   required_providers {                                                        # 필요한 프로바이더를 지정하여 Terraform이 해당 클라우드 또는 서비스와 상호작용할 수 있도록 함
-//     ncloud = {
-//       source = "NaverCloudPlatform/ncloud"
-//     }
-//   }
-//   required_version = ">= 0.13"                                                # Terraform 엔진의 최소 버전을 지정합니다.
-// }
+# Terraform 초기화
+terraform {
+  required_providers {
+    ncloud = {
+      source = "NaverCloudPlatform/ncloud"
+    }
+  }
+  required_version = ">= 0.13"
+}
 
-// locals {
-//   credentials = jsondecode(file("naver.json"))
-// }
+# 네이버 클라우드 프로바이더 설정
+provider "ncloud" {
+  access_key  = local.credentials.naver_v0.accessKeyId
+  secret_key  = local.credentials.naver_v0.secretKey
+  region      = "KR"
+  site        = "public"
+  support_vpc = true
+}
 
-// provider "ncloud" {
-//   access_key = local.credentials.naver_v0.accessKeyId
-//   secret_key = local.credentials.naver_v0.secretKey
-//   region     = "KR"
-//   site       = "public"
-//   support_vpc = true
-// }
+locals {
+  credentials = jsondecode(file("naver.json"))
+}
 
+# 네이버 클라우드 VPC 생성
+resource "ncloud_vpc" "ncp-vpc" {
+  ipv4_cidr_block    = "10.0.0.0/16"
+  name               = "ncp-vpc"
+}
 
-// # 네이버 클라우드 VPC 생성
-// resource "ncloud_vpc" "vpc" {
-//   ipv4_cidr_block = "172.16.0.0/16"                                           # VPC의 IP 대역 설정
-//   name            = "test-vpc"                                                # VPC의 이름
-// }
+# 네이버 클라우드 서브넷 생성
+resource "ncloud_subnet" "ncp-subnet" {
+  vpc_no             = ncloud_vpc.ncp-vpc.vpc_no
+  subnet             = "10.0.0.0/24"
+  zone               = "KR-2"
+  network_acl_no     = ncloud_vpc.ncp-vpc.default_network_acl_no
+  subnet_type        = "PUBLIC"
+  usage_type         = "GEN"
+  name               = "ncp-subnet"  
+}
 
-// # 네이버 클라우드 서브넷 생성
-// resource "ncloud_subnet" "public-subnet" {
-//   vpc_no         = ncloud_vpc.vpc.id
-//   subnet         = "172.16.10.0/24"                                           # 서브넷의 IP 대역 설정
-//   zone           = "KR-2"                                                     # 존 (Zone) 설정
-//   network_acl_no = ncloud_vpc.vpc.default_network_acl_no
-//   subnet_type    = "PUBLIC"                                                   # 서브넷 유형 (PUBLIC)
-//   name           = "public-a-subnet"                                          # 서브넷의 이름
-//   usage_type     = "GEN"                                                      # 사용 유형 (GEN)
-// }
+# 네이버 클라우드 Launch Configuration 생성
+resource "ncloud_launch_configuration" "ncp-lc" {
+  name                    = "nserver"  
+  server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"
+  # server_product_code      = "SVR.VSVR.HICPU.C002.M004.NET.SSD.B050.G002"  #고성능 cpu
+  server_product_code       = "SVR.VSVR.STAND.C002.M008.NET.SSD.B050.G002"   #표준 cpu
+}
 
-// # 네이버 클라우드 서버 생성
-// resource "ncloud_server" "public-server" {
-//   subnet_no                 = ncloud_subnet.public-subnet.id
-//   name                      = "test-web01"                                    # 서버의 이름
-//   # server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.B050"              # CentOS 이미지 사용 7.8버전
-//   server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0703.B050"              # CentOS 이미지 사용 7.3버전  3분58초
+# 네이버 클라우드 Auto Scaling Group 생성
+resource "ncloud_auto_scaling_group" "ncp-asg" {
+  access_control_group_no_list = [ncloud_access_control_group.ncp-acg.id]
+  subnet_no                    = ncloud_subnet.ncp-subnet.subnet_no
+  launch_configuration_no      = ncloud_launch_configuration.ncp-lc.launch_configuration_no
+  min_size                     = 1
+  max_size                     = 1
+  name                         = "ncp-autoscalinggroup"
+  server_name_prefix           = "nserver"  
+}
 
-//   # server_product_code       = "SVR.VSVR.HICPU.C002.M004.NET.HDD.B050.G002"  # 고성능 CPU 서버 사용        4분47초
-//   #  미입력시 가장낮은스펙적용   4분27초
+# 네이버 클라우드 액세스 컨트롤 그룹 생성
+resource "ncloud_access_control_group" "ncp-acg" {
+  name   = "ncp-accescontrlolgroup"
+  vpc_no = ncloud_vpc.ncp-vpc.vpc_no
+}
 
-//   login_key_name            = "ncp20231116"                                   # 로그인 키 이름
-// }
+# 네이버 클라우드 액세스 컨트롤 그룹 규칙 생성 (SSH 허용)
+resource "ncloud_access_control_group_rule" "ncp-acg-rule" {
+  access_control_group_no = ncloud_access_control_group.ncp-acg.id
 
+  inbound {
+    protocol    = "TCP"
+    ip_block    = "0.0.0.0/0"  # 모든 IP 허용 (실제 운영에서는 제한 필요)
+    port_range  = "22"
+    description = "SSH"
+  }
+}
 
-// # 네이버 클라우드 공인 IP 생성
-// resource "ncloud_public_ip" "public-ip" {                                     # 공인 IP는 외부에서 서버에 접근할 때 사용되는 IP 주소
-
-  
-//   server_instance_no = ncloud_server.public-server.id                         #  공인 IP를 어떤 서버와 연결할지를 설정
-// }
-
-// # 네이버 클라우드 액세스 컨트롤 그룹 생성
-// resource "ncloud_access_control_group" "test-acg" {                           # 액세스 컨트롤 그룹은 서버와 관련된 보안 규칙을 설정하는데 사용
-//   name   = "test-acg"                                                         # 이 액세스 컨트롤 그룹의 이름은 "test-acg"로 지정
-//   vpc_no = ncloud_vpc.vpc.id                                                  # 이 액세스 컨트롤 그룹을 만들 때 어떤 VPC를 사용할지를 설정
-// }
-
-// # 네이버 클라우드 액세스 컨트롤 그룹 규칙 생성
-// resource "ncloud_access_control_group_rule" "test-acg-rule" {                # 네이버 클라우드에서 액세스 컨트롤 그룹에 규칙을 추가하는 코드
-//   access_control_group_no = ncloud_access_control_group.test-acg.id          # 규칙을 어떤 액세스 컨트롤 그룹에 추가할지를 설정
-
-//   # SSH 트래픽만 허용
-//   inbound {                                                                  # 액세스 컨트롤 그룹 규칙에 SSH 트래픽을 허용하는 설정
-//     protocol    = "TCP"                                                      # TCP 프로토콜을 사용하며, 모든 IP 주소 (0.0.0.0/0)에서 SSH 포트 (22)로의 연결을 허용
-//     ip_block    = "0.0.0.0/0"
-//     port_range  = "22"
-//     description = "SSH"
-//   }
-// }
-
-// # 네이버 클라우드 라우팅 테이블 연결
-// resource "ncloud_route_table_association" "route_ass_public" {            # 네이버 클라우드의 라우팅 테이블을 설정하는 코드
-//   route_table_no = ncloud_vpc.vpc.default_public_route_table_no           # 어떤 라우팅 테이블과 어떤 서브넷을 연결할지를 설정
-//   subnet_no      = ncloud_subnet.public-subnet.id
-// }
+# # 네이버 클라우드 NAS 볼륨 생성
+# resource "ncloud_nas_volume" "ncp-my-nas" {
+#     volume_name_postfix            = "vol"  # NAS 볼륨의 이름에 붙일 접미사
+#     volume_size                    = "500"  # 볼륨 크기 (GB)
+#     volume_allotment_protocol_type = "NFS"  # 볼륨의 프로토콜 타입
+# }
 
   
